2c85544605c03ebe545b6255b7aee3eb54de836c
Dedicatus546 <1607611087@qq.com>
2024-02-04 08:59:46 +0000

    Deploying to git-page from @ Dedicatus546/vuetify-message-vue3@6182ae0470cf75727e166123f83ace620dcf7ce8 🚀

---
+++ assets/demo.md.UZqkVmvO.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./chunks/framework.o3km4ULQ.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"demo.md","filePath":"demo.md"}'),a={name:"demo.md"};function r(s,c,n,d,m,p){return o(),t("div")}const f=e(a,[["render",r]]);export{i as __pageData,f as default};

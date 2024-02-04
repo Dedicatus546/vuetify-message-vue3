13b303d7b01130f01350b940c6ff9a06994146ae
Par9uet <1607611087@qq.com>
2024-02-04 18:05:14 +0800

    chore: 删除无用文件

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import "vuetify/styles";
-
-import { createApp } from "vue";
-import { createVuetify } from "vuetify";
-
-import App from "./App.vue";
-
-createApp(App).use(createVuetify()).mount("#app");
